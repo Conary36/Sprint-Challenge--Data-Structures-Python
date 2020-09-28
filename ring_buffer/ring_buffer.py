e87289335354adfa6e67fb5132700e2904b76af4
Conary36 <cbeckford144@gmail.com>
2020-09-27 22:11:33 -0400

    ...

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,20 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity # max amount
-        self.buffer = [] # created list
-        self.i = 0 # 0 set as initial index
+        self.capacity = capacity  # max amount
+        self.buffer = []  # created list
+        self.i = 0  # 0 set as initial index
 
     """append an element at the end of the buffer"""
+
     def append(self, item):
         if len(self.buffer) < self.capacity:
             self.buffer.append(item)
         else:
             self.buffer[self.i] = item
             if self.i == self.capacity - 1:
-                self.i = (self.i + 1) % self.capacity # remaining value set as index
+                self.i = (self.i + 1) % self.capacity  # remaining value set as index
             else:
-                self.i += 1 # increment by 1
+                self.i += 1  # increment by 1
 
     """ Return a list of elements from the oldest to the newest. """
+
     def get(self):
         return self.buffer
